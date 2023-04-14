6cdaa450078bfbb2dfd4f77a8ce30145ec4229e9
ColtonWehking <cawehking@gmail.com>
2023-04-14 15:19:15 -0500

    Create test.js

---
+++ scripts/test.js
@@ -0,0 +1,9 @@
+var exec = require('child_process').execSync;
+
+function main() {
+  const arguments = process.argv.slice(2);
+  const fileNames = arguments[0].split(',');
+  let val = exec('find ~force-app/ *.cls')
+  process.stdout.write(val);
+}
+main();
