f86015a47c7c302cb1c2a7551aa7fcb236e685f1
ColtonWehking <cawehking@gmail.com>
2023-04-14 15:28:14 -0500

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -3,7 +3,7 @@ var exec = require('child_process').execSync;
 function main() {
   //const arguments = process.argv.slice(2);
   //const fileNames = arguments[0].split(',');
-  let val = exec('find $(pwd)/force-app/ *.cls')
+  let val = exec('find $(pwd)/force-app/* *.cls')
   process.stdout.write(val);
 }
 main();
