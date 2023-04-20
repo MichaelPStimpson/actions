c6221372c3bd9fdd56cfa78ace09dd43cb1ff124
ColtonWehking <cawehking@gmail.com>
2023-04-20 10:44:57 -0500

    Update my-test.js

--- scripts/my-test.js
+++ scripts/my-test.js
@@ -21,10 +21,6 @@ let paths = [];
 	
 
     //exec('rm -r $(pwd)/temp && mkdir temp');
-	
-
-    process.stdout.write('4');
-	
 
     exec('mkdir temp');
 	
@@ -32,23 +28,13 @@ let paths = [];
     fileNames.forEach((name) => {
 	
 
-        process.stdout.write('5');
-	
-
         exec('find $(pwd)/force-app/* -name "' + name + '.cls" > ~/searchResults.txt');
-
-        process.stdout.write('6');
-	
-
-        process.stdout.write(val);
-	
-
-        process.stdout.write('7');
 	
 
         exec('cp (cat ~searchResults.txt) "$(pwd)/temp/'  + name + '"');
 	
 
+	console.log('copied', name);
         process.stdout.write('Copied ' + name);
 	
 
