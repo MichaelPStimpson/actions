323b312b9e97c6d2c26c57f441c5b284b3d64f74
ColtonWehking <cawehking@gmail.com>
2023-04-20 10:18:10 -0500

    Update my-test.js

--- scripts/my-test.js
+++ scripts/my-test.js
@@ -1,3 +1,91 @@
-const xml2js = require('xml2js');
-const xmlBufferToString = require('xml-buffer-tostring');
-process.stdout.write('colton says hello');
+var exec = require('child_process').execSync(
+	
+
+    'rsync -avAXz --info=progress2 "/src" "/dest"',
+	
+
+    {stdio: 'inherit'}
+	
+
+);
+	
+
+ 
+	
+
+function main() {
+	
+
+    // Parse first argument
+	
+
+    process.stdout.write('1');
+	
+
+    const arguments = process.argv.slice(2);
+	
+
+    process.stdout.write('2');
+	
+
+    const fileNames = arguments[0].split(',');
+	
+
+    process.stdout.write('3');
+	
+
+    const pwd = arguments[1];
+	
+
+    let paths = [];
+	
+
+   
+	
+
+    // Clear the temp folder
+	
+
+    //exec('rm -r $(pwd)/temp && mkdir temp');
+	
+
+    process.stdout.write('4');
+	
+
+    exec('mkdir temp');
+	
+
+    fileNames.forEach((name) => {
+	
+
+        process.stdout.write('5');
+	
+
+        let val = exec('find $(pwd)/force-app/* -name "' + name + '.cls"');
+	
+
+        process.stdout.write('6');
+	
+
+        process.stdout.write(val);
+	
+
+        process.stdout.write('7');
+	
+
+        exec('cp "' + val + '" "$(pwd)/temp/'  + name + '"');
+	
+
+        process.stdout.write('Copied ' + name);
+	
+
+    });
+	
+
+}
+	
+
+ 
+	
+
+main();
